601dd2466493ded27f3b06d9d7ca62261a20e623
Kārlis Goba <karlis.goba@gmail.com>
2016-03-03 16:27:18 +0200

    Switched to common AudioPlayer implementation

--- nodes/PBX/line.h
+++ nodes/PBX/line.h
@@ -3,20 +3,7 @@
 #include "config.h"
 #include <Common/util.h>
 #include <Common/ws2803s.h>
-
-
-class AudioPlayer {
-public:
-  AudioPlayer(WS2803S &expander, byte pin0, byte pin1, byte pin2) 
-    : _expander(expander), _pin0(pin0), _pin1(pin1), _pin2(pin2) {}
-  
-  void setup();
-  void play(byte id);
-private:
-  byte _pin0, _pin1, _pin2;
-  WS2803S &_expander;
-};
-
+#include <Common/audioplayer.h>
 
 struct PLineConfig
 {
